80747ae5ab5282c90a324cb9bd6e50c2bbdeb064
Paulo Azoia <pazoia.gomes@gmail.com>
2021-09-26 16:26:09 +0100

    Pictures being loaded into panels
    when clicked they expand to fullscreen

--- src/Panels.js
+++ src/Panels.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import "./Panels.css";
 
 export const Panels = () => {
+  const [selectedPanel, setSelectedPanel] = useState(null);
+  
+  const handleClick = (panelColor) => {
+    if (!selectedPanel) {
+      setSelectedPanel(panelColor);
+    } else {
+      setSelectedPanel(null);
+    }
+  };
+
+  if (!selectedPanel) {
+    return (
+      <div className="panels">
+        <Panel handleClick={handleClick} panelColor="https://source.unsplash.com/gYl-UtwNg_I/3000x1500" />
+        <Panel handleClick={handleClick} panelColor="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=3000&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d" />
+        <Panel handleClick={handleClick} panelColor="https://source.unsplash.com/rFKUFzjPYiQ/3000x1500" />
+      </div>
+    );
+  }
+
   return (
     <div className="panels">
-      <Panel panelColor="Orange" />
-      <Panel panelColor="Green" />
-      <Panel panelColor="Grey" />
+      <Panel handleClick={handleClick} panelColor={selectedPanel} />
     </div>
   );
 };
 
-const Panel = (props) => {
+const Panel = ({ panelColor, handleClick }) => {
   return (
-    <div className="panel" style={{
-        backgroundColor: props.panelColor
-    }}>
-      {props.panelColor}
-    </div>
+    <div
+      onClick={() => handleClick(panelColor)}
+      className="panel"
+      style={{
+        backgroundImage: `url(${panelColor})`,
+      }}
+    />
   );
 };

2253bf87b262f8e03ddfac7fa1001763eef4ab94
Paulo Azoia <pazoia.gomes@gmail.com>
2021-11-16 14:33:46 +0000

    moved data to be displayed whithin the panel into it's own file:
    this will allow me to add more data into an array and choose at ramdom diferent data sets

---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const themeNames = {
+    beach: "By the Beach",
+    mountain: "Walking in the Mountains",
+    city: "A day in the City",
+};
+
+export const positiveAffirmations = {
+    life: `You can do this. Be patient and persistent.`,
+};
+
+export const panelImage = {
+    beach: "https://source.unsplash.com/eXHeq48Z-Q4/3000x1500",
+    mountain: "https://source.unsplash.com/7bwQXzbF6KE/3000x1500",
+    city: "https://source.unsplash.com/kheTI8pIywU/3000x1500",
+}

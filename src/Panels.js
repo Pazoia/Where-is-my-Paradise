e5d3db3206958e550d0557d309c9fe5a3b5b716c
Paulo Azoia <pazoia.gomes@gmail.com>
2021-11-16 14:42:49 +0000

    selected image and text update:
    same image being used once a user selects and clicks on an image,
    dquote> theme names get replaced by a positive affirmation

--- src/Panels.js
+++ src/Panels.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import React from "react";
-import "./Panels.css";
+
+import "./css/Panels.css";
+import { Panel } from "./Panel";
+import { themeNames, panelImage } from "./data";
 
 export const Panels = () => {
   const [selectedPanel, setSelectedPanel] = useState(null);
@@ -13,42 +16,14 @@ export const Panels = () => {
     }
   };
 
-  if (!selectedPanel) {
-    return (
-      <div className="wrapper">
-        <h1 className="title">Where is your Paradise?</h1>
-        <div className="panels">
-          <Panel handleClick={handleClick} panelImage="https://source.unsplash.com/eXHeq48Z-Q4/3000x1500" theme="By the Beach" />
-          <Panel handleClick={handleClick} panelImage="https://source.unsplash.com/7bwQXzbF6KE/3000x1500" theme="Walking in the Mountains" />
-          <Panel handleClick={handleClick} panelImage="https://source.unsplash.com/kheTI8pIywU/3000x1500" theme="A day in the City" />
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="wrapper">
-      <text className="positive-affirmation">
-        My life is just beginning. <br/>
-        Everything will work out for me.
-      </text>
+      <h1 className="title">Where is your Paradise?</h1>
       <div className="panels">
-          <Panel handleClick={handleClick} panelImage={selectedPanel} />
+        <Panel selectedPanel={selectedPanel} handleClick={handleClick} panelImage={panelImage.beach} theme={themeNames.beach} />
+        <Panel selectedPanel={selectedPanel} handleClick={handleClick} panelImage={panelImage.mountain} theme={themeNames.mountain} />
+        <Panel selectedPanel={selectedPanel} handleClick={handleClick} panelImage={panelImage.city} theme={themeNames.city} />
       </div>
-    </div>
-  );
-};
-
-const Panel = ({ panelImage, handleClick, theme }) => {
-  return (
-    <div
-      onClick={() => handleClick(panelImage)}
-      className="panel"
-      style={{
-      backgroundImage: `url(${panelImage})`,
-      }}
-    >
-      <h1 className="theme">{theme}</h1>
     </div>
   );
 };
